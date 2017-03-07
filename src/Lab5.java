5e107c07feba3d3186787ff6702de03912c90f2c
jessehartloff <hartloff@buffalo.edu>
2017-03-07 08:23:49 -0500

    typo

--- src/Lab5.java
+++ src/Lab5.java
@@ -3,7 +3,7 @@ public class Lab5 {
 
 
     // 1. Write a method that takes a country code, city name, and region as parameters and prints its latitude and longitude
-	//    -The country code determines which file you will open. The city name determines which line is needed
+    //    -The country code determines which file you will open. The city name determines which line is needed
 
 	
     // 2. Write a method that takes 2 cities by country code, city name, and region and returns the greater circle distance
